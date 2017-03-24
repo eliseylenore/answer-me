4173b091820d7c3a0f4452c59dabd8037048ee44
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-24 12:12:05 -0700

    Create ability to add answers to specific questions and display them on question page

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('question', params.question_id);
+      return this.store.findRecord('question', params.question_id);
   },
 
   actions: {
@@ -14,6 +14,15 @@ export default Ember.Route.extend({
       });
       question.save();
       this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      })
+      this.transitionTo('question', question);
     }
   }
 });
